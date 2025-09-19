0455a53b8f241dc2f4cc22cad4437b7ffc35ca77
GyulyVGC <gyulyvgc99@gmail.com>
2025-09-19 11:59:28 +0200

    update client_authentication library to print token in debug mode

--- client_authentication/src/control_channel/commands/update_token_command.rs
+++ client_authentication/src/control_channel/commands/update_token_command.rs
@@ -14,6 +14,8 @@ impl UpdateTokenCommand {
 impl ExecutableCommand for UpdateTokenCommand {
     async fn execute(self) -> Result<(), nullnet_liberror::Error> {
         log::debug!("Received UpdateTokenCommand");
+        #[cfg(debug_assertions)]
+        log::trace!("TOKEN: {}", self.token);
         self.context.token_provider.update(self.token).await;
         Ok(())
     }
