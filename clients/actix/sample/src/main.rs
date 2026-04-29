a3c4a18bef26d5f82d0b28a7de936b326e0cb42d
GyulyVGC <gyulyvgc99@gmail.com>
2026-04-29 17:40:31 +0200

    fix actix-sample for local testing

--- clients/actix/sample/src/main.rs
+++ clients/actix/sample/src/main.rs
@@ -49,6 +49,8 @@ async fn remote_color(conn: web::Data<Mutex<TimestampConn>>) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     env_logger::init();
 
+    tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+
     println!("Connecting to timestamp server at {TIMESTAMP_SERVER}");
     let stream = TcpStream::connect(TIMESTAMP_SERVER).await?;
     let (r, w) = stream.into_split();
